25c8e559148e6eaf633beeee1b09e2d0c42684f7
Pepijn Olivier <olivier.pepijn@gmail.com>
2016-01-12 16:01:00 +0100

    added translations
    Signed-off-by: Pepijn Olivier <olivier.pepijn@gmail.com>

---
+++ Resources/lang/en/users.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'title' => [
+        'users' => 'user',
+        'create user' => 'Create a user',
+        'edit user' => 'Edit a user',
+    ],
+    'button' => [
+        'create user' => 'Create a user',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+];
